13c55dd99c59029016fb005ca3e8714d4d9c7d5b
yasyir90 <yasyirmasyal@gmail.com>
2024-09-12 07:36:31 +0700

    add project and slice part 1

---
+++ portofolio/src/home/home.jsx
@@ -0,0 +1,71 @@
+import "./home.css"
+
+function Home(){
+    return(
+        <>
+        
+     <div className="container_home">
+
+        <div className="siang">
+        <header>
+
+        <img className="matahari" src="/image/afternoon/sun_7218584.png" alt="matahari" />
+        <img className="awan2" src="/image/afternoon/clouds_2722836.png" alt="awan" />
+
+        <img className="balonUdara" src="/image/afternoon/hot-air-balloon_2723893.png" alt="balon udara" />
+
+        <img className="awan4" src="/image/afternoon/icloud_2186377.png" alt="awan" />
+        <img className="pesawat1" src="/image/afternoon/aeroplane_2723741.png" alt="pesawat" />
+
+
+
+        <img className="awan1" src="/image/afternoon/cloud-network_2185940.png" alt="awan" />
+        <img className="balon" src="/image/afternoon/balloon_2723852.png" alt="balon" />
+
+        <img className="awan3" src="/image/afternoon/clouds_2722874.png" alt="awan" />
+        <img className="zepline" src="/image/afternoon/zeppelin_2723746.png" alt="zepline" />
+        <img className="pesawat2" src="/image/afternoon/aeroplane_2723812.png" alt="pesawat" />
+
+        </header>
+
+        <div className="content">
+        <img className="orang" src="/image/afternoon/orang.png" alt="orang" />
+
+        <img className="rumah7" src="/image/building/rumah 7.png" alt="rumah" />
+        <img className="rumah8" src="/image/building/rumah 8.png" alt="rumah" />
+        <img className="rumah2" src="/image/building/rumah 2.png" alt="rumah" />
+        </div>
+
+        
+        <footer>
+
+        <img className="jalanHome" src="/image/building/jalan.png" alt="jalan" />
+        <img className="payung" src="/image/afternoon/beach_12791365.png" alt="payung" />
+        <img className="bangku" src="/image/afternoon/horizontal_12791469.png" alt="bangku" />
+
+
+        </footer>
+
+        </div>
+
+        <div className="malam">
+
+        body
+
+        </div>
+
+
+        <div className="luarAngkasa">
+        footer
+        </div>
+
+
+
+        
+        </div>
+        </>
+    );
+
+}
+
+export default Home;
